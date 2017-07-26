cf9a86ab336268a98bde7597f585a17a93521b8e
Alexander Kuznetsov <kuzn3c0v@gmail.com>
2017-07-26 13:19:22 +0300

    Initial commit

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\UserBundle\FOSUserBundle;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+use AppBundle\Entity\User;
+
+class UserController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/user")
+     */
+    public function getAction()
+    {
+        $restresult = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
+
+        if ($restresult === null) {
+            return new View("there are no users exist", Response::HTTP_NOT_FOUND);
+        }
+        return $restresult;
+    }
+
+    /**
+     * @Rest\Delete("/user/{id}")
+     */
+    public function deleteAction($id)
+    {
+        $sn = $this->getDoctrine()->getManager();
+        $user = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+        if (empty($user)) {
+            return new View("user not found", Response::HTTP_NOT_FOUND);
+        }
+        else {
+            $sn->remove($user);
+            $sn->flush();
+        }
+        return new View("deleted successfully", Response::HTTP_OK);
+    }
+}
